901b20f24c6a6c5c3a199ea26d0e7b3372f0fd80
Brad Hanks <hanks.brad@gmail.com>
2021-12-15 12:36:19 -0700

    Update index.js

--- frontend/index.js
+++ frontend/index.js
@@ -3,10 +3,9 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import store from './app/store/configureStore';
 import App from './app/App';
-import { loadCSSFromURLAsync, loadCSSFromString } from '@airtable/blocks/ui';
-import styles from './app/styles/main.scss'
+import { loadCSSFromURLAsync } from '@airtable/blocks/ui';
 loadCSSFromURLAsync('https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css');
-loadCSSFromString(styles);
+loadCSSFromURLAsync('./app/styles/main.scss');
 
 function HelloWorldBlock() {
     // YOUR CODE GOES HERE
