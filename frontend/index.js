c2cbd4fe7d81264d2b9e24b7e8673b925b0da06d
Brad Hanks <hanks.brad@gmail.com>
2021-12-15 12:41:01 -0700

    Update index.js

--- frontend/index.js
+++ frontend/index.js
@@ -5,7 +5,7 @@ import store from './app/store/configureStore';
 import App from './app/App';
 import { loadCSSFromURLAsync } from '@airtable/blocks/ui';
 loadCSSFromURLAsync('https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css');
-loadCSSFromURLAsync('./app/styles/main.scss');
+// loadCSSFromURLAsync('./app/styles/main.scss');
 
 function HelloWorldBlock() {
     // YOUR CODE GOES HERE
